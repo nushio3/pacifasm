0a482d90f710668a6823e2d09785df89c5c19d0a
Takayuki Muranushi <muranushi@gmail.com>
2013-08-13 11:39:24 +0900

    new makefiles

--- src/Asm.hs
+++ src/Asm.hs
@@ -1,2 +1,9 @@
 module Asm where
 
+import Data.SBV
+
+data Op = Var | Imm 
+        | Not | Shl1 | Shr1 | Shr4 | Shr16 
+        | And | Or | Xor | Plus
+                           
+                           
