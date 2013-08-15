f0e3a4b25cdd3643785d0a5ba0b2931240c74c8a
Takayuki Muranushi <muranushi@gmail.com>
2013-08-15 17:11:13 +0900

    fix bug in the spec.

--- exe-src/p22.hs
+++ exe-src/p22.hs
@@ -5,7 +5,9 @@ import Data.SBV
 main :: IO ()
 main = synthesizeProgram [Plus, Shr1, Shr4, Shr16, Or, Imm, Jmp] myProp
 
+-- round up to the next highest power of 2
+
 myProp :: ProgramProperty
 myProp alpha beta =
-  ite (msb alpha) (beta .==0) (sbvPopCount beta .==1 &&& beta .<= 2*alpha &&& beta .> alpha)
+  ite (msb alpha ||| alpha.==0) (beta .==0) (sbvPopCount beta .==1 &&& beta .<= 2*alpha &&& beta .> alpha)
 
